f07bdf65ffe2b726cb825bb5dd104d23c4ed36fd
teppei0307 <44926905+teppei0307@users.noreply.github.com>
2023-07-29 18:11:12 +0800

    add: Prettier config file

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: false,
+  trailingComma: "all",
+  formatOnSave: true,
+};
+
+export default config;

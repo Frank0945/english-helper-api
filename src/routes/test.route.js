4d4b0feae7edceeb9eca488898dbe7ca3ab2071a
teppei0307 <44926905+teppei0307@users.noreply.github.com>
2023-07-30 12:07:03 +0800

    remove: Delete test files (controller, model, route, service)

--- src/routes/test.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/test.controller");
-
-router.get("/", controller.getUserInfo);
-
-module.exports = router;

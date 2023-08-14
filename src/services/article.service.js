9fb469f18f3d2ba9ea040873c6f71eb5483f2faa
Frank <40611willy@gmail.com>
2023-08-14 19:18:18 +0800

    chore: Add doc comment in article service

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -1,10 +1,17 @@
 /**
  * @param {{vocs: Array<Array<string>>}} data
+ * @returns {Array<{
+ *  title: string,
+ *  content: string,
+ *  vocs: string,
+ *  ans: Array<string>,
+ *  correctAns: number,
+ * }>}
  */
 async function generateArticle(data) {
   // TODO: Synergize with chatGPT
   console.log(data.vocs);
-  return data.vocs;
+  return [];
 }
 
 module.exports = {

4498c4a21887a5b0e0e779bf1ced4382b44d1a9c
Frank <40611willy@gmail.com>
2023-11-12 11:32:56 +0800

    chore: 40s connect timeout to DB dialect options

--- src/services/_db.service.js
+++ src/services/_db.service.js
@@ -4,11 +4,15 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DATABASE,
   process.env.DB_USERNAME,
-  process.env.DB_PASSWORD, {
+  process.env.DB_PASSWORD,
+  {
     dialect: "mariadb",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-  }
+    dialectOptions: {
+      connectTimeout: 40 * 1000, // 40 seconds connect timeout
+    },
+  },
 );
 
 sequelize
